cd4ca1e11e7d5d497fa7b33101bad7a3edc5412f
Evan Merzon <evandaniel160@yahoo.com>
2020-07-10 23:36:16 -0400

    Added migration scripts

--- flaskMigrate/app.py
+++ flaskMigrate/app.py
@@ -1,3 +1,15 @@
+'''
+USE MIGRATION LIBRARY:
+
+command line:
+1. flask db init --> Creates initial migration structure
+2. DROP DB --> dropdb {database name}
+    a. If this doesn't work, try to turn of postgreSQL (pg_ctl -D /usr/local/var/postgres stop) and then start it again 
+    (pg_ctl -D /usr/local/var/postgres start)
+3. flask db migrate --> Creates migration file w/ upgrade/downgrade logic set up  (use when want to apply change or set up file to upgrade/downgrade)
+OTHER COMMANDS: flask db upgrade --> Runs upgrade function in migration file to apply upgrade
+OTHER COMMANDS: flask db downgrade --> Runs downgrade function in migration file to de-apply upgrade
+'''
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -7,16 +19,20 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://evanmerzon@localhost:5432/to
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+#Use migration class
 migrate = Migrate(app, db)
+
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean(), nullable=True, default=True)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
-db.create_all()
+#DON'T NEED db.create_all()
+#db.create_all()
 #db.session.add(Todo(description='Have a good day'))
 #db.session.commit()
 @app.route("/todos/create", methods=['POST'])
