8dfb41d22401a137bb91e927cefd8d480f187e4a
Evan Merzon <evandaniel160@yahoo.com>
2020-07-18 19:37:41 -0400

    ???

--- trivia_api/frontend/src/components/FormView.js
+++ trivia_api/frontend/src/components/FormView.js
@@ -24,7 +24,7 @@ class FormView extends Component {
         return;
       },
       error: (error) => {
-        alert('Unable to load categories. Please try your request again')
+        alert(error.message)
         return;
       }
     })
